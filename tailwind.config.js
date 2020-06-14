0a2710876d6dead0a37e65f0fb58b6cb6d430ba7
Thorsten Luenborg <t.luneborg@googlemail.com>
2020-06-14 12:14:29 +0200

    deps: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  // purge: ['./dist/**/*.css'],
+  purge: false,
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+};
